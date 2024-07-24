195329151f53c8103ff2d0247514214dc3cc55af
Kayla Hunt <huntkay1@msu.edu>
2024-07-24 19:22:17 -0400

    Style card

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,18 +1,26 @@
-
+import normalIcon from './normal.png'
 export default function Card({ data }) {
     
+    function formatString(string) {
+
+    }
     
     return (
         <div className='card'>
             <div className='card-contents'>
                 <div className='card-header'>
-                <h2>{data.name}</h2>
-                    <h2>HP <span>{data.stats[0].base_stat}</span></h2>
+                    <h2>{data.name}</h2>
+                    <h2><span>HP</span> {data.stats[0].base_stat}</h2>
                 </div>
-                <img src={data.sprites.front_default}></img>
+                <img src={data.sprites.other['official-artwork'].front_default} alt={data.name}></img>
                 <div className='card-attacks'>
-                {data.abilities.map(function (item, index, array) {
-                        return <h3 key={index}>{item.ability.name}</h3>
+                {data.abilities.map((item, index) => {
+                        return (
+                            <div className='attack-container'>
+                                <img src={normalIcon}></img>
+                                <p key={index}>{item.ability.name}</p>
+                            </div>
+                        )
                     })}
                 </div>
             </div>
