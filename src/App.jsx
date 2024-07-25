f9243d46709ecda119ac939f116f747b0aab151c
Kayla Hunt <huntkay1@msu.edu>
2024-07-25 16:09:56 -0400

    Create multiple cards
    Using the card component, create a card for
    each pokemon in the array

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,23 @@ function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/ditto`) 
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`) 
       .then(response => response.json())
       .then(data => setData(data))
       .catch(error => console.error(error))
     
   }, [])
 
-  console.log(data);
   return (
     <>
       {data != null && 
-        <Card 
-          data={data}
-        />
+        data.results.map((pokemon, index) => (
+          <Card 
+          pokemonName={pokemon.name}
+          key={index}
+          />
+        ))
       }
-
-
     </>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,29 +1,56 @@
-import normalIcon from './normal.png'
-export default function Card({ data }) {
-    
-    function formatString(string) {
+import normalIcon from './normal.png';
+import { useState, useEffect } from 'react';
 
+export default function Card({ pokemonName }) {
+
+    const [data, setData] = useState(null);
+    let hp;
+    let imgURL;
+    let moves;
+    let type;
+
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`) 
+          .then(response => response.json())
+          .then(data => setData(data))
+          .catch(error => console.error(error))
+        
+    }, [data])
+
+    if (data != null) {
+        hp = data.stats[0].base_stat;
+        imgURL = data.sprites.other['official-artwork'].front_default;
+        moves = data.moves.slice(0,3); //only want to use 3 moves
+        type = data.types[0].type.name;
     }
-    
+
+
     return (
-        <div className='card'>
-            <div className='card-contents'>
-                <div className='card-header'>
-                    <h2>{data.name}</h2>
-                    <h2><span>HP</span> {data.stats[0].base_stat}</h2>
-                </div>
-                <img src={data.sprites.other['official-artwork'].front_default} alt={data.name}></img>
-                <div className='card-attacks'>
-                {data.abilities.map((item, index) => {
-                        return (
-                            <div className='attack-container'>
-                                <img src={normalIcon}></img>
-                                <p key={index}>{item.ability.name}</p>
-                            </div>
-                        )
-                    })}
+        <>
+        {data != null && 
+            <div className='card'>
+                <div className='card-contents'>
+                    <div className='card-header'>
+                        <h2>{pokemonName}</h2>
+                        <h2><span>HP</span> {hp}</h2>
+                    </div>
+
+                    <img src={imgURL} alt={pokemonName} className='card-hero'></img>
+
+                    <div className='card-attacks'>
+                        {moves.map((item, index) => {
+                            return (
+                                <div key={index} className='attack-container'>
+                                    <img src={normalIcon}></img>
+                                    <p>{item.move.name}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
+        }
+        </>
+       
     )
 }
