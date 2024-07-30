4a9b0d34affb92368686033a7c4ef30ab83cb13d
Kayla Hunt <huntkay1@msu.edu>
2024-07-30 19:25:34 -0400

    Add 'you lose' message

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,23 @@ function App() {
   const [score, setScore] = useState(0);
   const [selectedCards, setSelectedCards] = useState([]);
   const [flipCards, setFlipCards] = useState('card-inner flipped');
+  const [showLoseMessage, setShowLoseMessage] = useState(false);
 
   window.addEventListener('load', () => setTimeout(() => setFlipCards('card-inner'), 300)); //flip cards over on first load
 
   function makeMove(cardName) {
     let loss = checkForLoss(cardName);
     if (loss) {
-      console.log('you lose');
+      setShowLoseMessage(true);
       setScore(0);
       setSelectedCards([]);
     } else {
+      setTimeout(() => setFlipCards('card-inner'), 50); //flip cards to front
       setSelectedCards([...selectedCards, cardName]);
       addPoint();
       shuffleDeck(data);
-      setTimeout(() => setFlipCards('card-inner'), 50); //flip cards to front
     }
   }
-
-
 
   function checkForLoss(cardName) {
     if(selectedCards.includes(cardName)) {
@@ -43,7 +42,7 @@ function App() {
   useEffect(() => {
       const fetchData = async() => {
         try {
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`);
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`);
             const data = await response.json();
             setData(data);
         } catch (error) {
@@ -76,6 +75,7 @@ function App() {
     <>
       <Scoreboard 
       score={score}
+      showLoseMessage={showLoseMessage}
       />
       <div id='cards-container'>
         {displayedDeck != null && 
--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -1,8 +1,10 @@
-export default function Scoreboard({ score }) {
+export default function Scoreboard({ score, showLoseMessage }) {
 
     return(
         <div id='scoreboard-container'>
             <p id='score'>Current Score: {score}</p>
+            {showLoseMessage && 
+            <p id='lose-message'>You Lose!</p>}
         </div>
     )
 }
